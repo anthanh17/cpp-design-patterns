933af077204599b5d0a156481850d4bbe41b8c5a
Annt <annguyenthe.work@gmail.com>
2023-06-21 21:39:03 +0700

    update singleton

--- creational-patterns/singleton/singleton.cc
+++ creational-patterns/singleton/singleton.cc
@@ -4,43 +4,47 @@
  * 2023
  */
 
-#include <string>
 #include <iostream>
+#include <memory>
+#include <string>
 
 class Database {
-public:
-  Database(const Database&) = delete;
-  Database& operator=(const Database&) = delete;
+ public:
+  Database(const Database &) = delete;
+  Database &operator=(const Database &) = delete;
   ~Database() = default;
 
-  static Database *GetInstance(std::string name) {
+  static std::unique_ptr<Database> GetInstance(std::string name) {
     if (nullptr == instance_ptr_) {
-      instance_ptr_ = new Database(name);
+      instance_ptr_ = std::make_unique<Database>(name);
+      std::cout << "\nKhoi tao instance\n";
     }
-    return instance_ptr_;
+    return std::move(instance_ptr_);
   }
 
-  std::string GetName() const {
-    return name_;
-  }
-private:
-  static Database *instance_ptr_;
+  friend std::unique_ptr<Database> std::make_unique<Database>(std::string &);
+
+  std::string GetName() const { return name_; }
+
+ private:
+  static std::unique_ptr<Database> instance_ptr_;
   std::string name_;
 
   explicit Database(std::string name) : name_{name} {}
 };
 
-Database *Database::instance_ptr_ = nullptr;
+std::unique_ptr<Database> Database::instance_ptr_{nullptr};
 
 int main() {
-  Database *database1 = Database::GetInstance("Products");
-  std::cout << "This is the " << database1->GetName() << " database." << std::endl;
+  std::unique_ptr<Database> database1 = Database::GetInstance("Products");
+  std::cout << "This is the " << database1->GetName() << " database."
+            << std::endl;
 
-  Database *database2 = Database::GetInstance("Test");
-  std::cout << "This is the " << database2->GetName() << " database." << std::endl;
+  std::unique_ptr<Database> database2 = Database::GetInstance("Test");
+  std::cout << "This is the " << database2->GetName() << " database."
+            << std::endl;
   return 0;
 }
-
 
 /*
 Output:
