bca706353143dd776138b8078dc8344722a64795
annt <annt@coccoc.com>
2023-06-22 16:48:53 +0700

    update singleton

--- creational-patterns/singleton/singleton.cc
+++ creational-patterns/singleton/singleton.cc
@@ -6,7 +6,7 @@
 
 #include <iostream>
 #include <memory>
-#include <string>
+#include <string_view>
 
 class Database {
  public:
@@ -14,33 +14,39 @@ class Database {
   Database &operator=(const Database &) = delete;
   ~Database() = default;
 
-  static std::unique_ptr<Database> GetInstance(std::string name) {
+  static Database* GetInstance(std::string_view name) {
     if (nullptr == instance_ptr_) {
-      instance_ptr_ = std::make_unique<Database>(name);
-      std::cout << "\nKhoi tao instance\n";
+      // Since the unique_ptr class is not a member of the Database class,
+      // it is not possible to call the Database constructor in private
+      // Here is a way to make unique_ptr callable to Database constructor
+      instance_ptr_ = std::unique_ptr<Database>(new Database(name));
     }
-    return std::move(instance_ptr_);
+    // Cannot use std::move unique_ptr because it will assign instance_ptr_ = null_ptr
+    return instance_ptr_.get();
   }
 
-  friend std::unique_ptr<Database> std::make_unique<Database>(std::string &);
+  // The second way is to let the std::make_shared constructor be a friend of
+  // the Database class so that you can call the private property of the Database.
+  // friend std::unique_ptr<Database> std::make_unique<Database>(std::string&);
 
-  std::string GetName() const { return name_; }
+  std::string_view GetName() const { return name_; }
 
  private:
   static std::unique_ptr<Database> instance_ptr_;
-  std::string name_;
+  std::string_view name_;
 
-  explicit Database(std::string name) : name_{name} {}
+  explicit Database(std::string_view name) : name_{name} {}
 };
 
+// Static variables must be initialized outside the class area
 std::unique_ptr<Database> Database::instance_ptr_{nullptr};
 
 int main() {
-  std::unique_ptr<Database> database1 = Database::GetInstance("Products");
+  const auto database1 = Database::GetInstance("Products");
   std::cout << "This is the " << database1->GetName() << " database."
             << std::endl;
 
-  std::unique_ptr<Database> database2 = Database::GetInstance("Test");
+  const auto database2 = Database::GetInstance("Test");
   std::cout << "This is the " << database2->GetName() << " database."
             << std::endl;
   return 0;
